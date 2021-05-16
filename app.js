1930f4cbb35397aae77c5691718411ef9ca88fd9
JuanSBonora <juanfcosantiagob1002@outlook.com>
2021-05-16 00:37:47 -0500

    Literal Types

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combineAges = combine(15, 10);
+var combineAges = combine(15, 10, 'as-number');
 console.log(combineAges);
-var combineNames = combine('Max', 'Ana');
+var combinedStringAges = combine('15', '10', 'as-number');
+console.log(combinedStringAges);
+var combineNames = combine('Max', 'Ana', 'as-text');
 console.log(combineNames);
