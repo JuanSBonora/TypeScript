56c972551ed66c3c9fc257db51d19d9e8996cd07
JuanSBonora <juanfcosantiagob1002@outlook.com>
2021-05-16 03:28:31 -0500

    return Types &'Void'

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-var u1 = { name: 'Max', age: 30 };
-function greet(user) {
-    return ('Hi my name is ' + user.name);
+function add(n1, n2) {
+    return n1 + n2;
 }
-function isOlder(user, checkAge) {
-    if (checkAge > user.age) {
-        console.log('is Older');
-    }
-    else {
-        console.log('is Young');
-    }
-    return checkAge > user.age;
+function printResult(num) {
+    console.log('Result: ' + num);
 }
-var r1 = greet(u1);
-console.log(r1);
-var r2 = isOlder(u1, 15);
-console.log(r2);
+printResult(add(5, 6));
