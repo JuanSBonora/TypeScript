ba5096eb4423d5e535c65a77d21f319ea92ab869
JuanSBonora <juanfcosantiagob1002@outlook.com>
2021-05-15 20:53:45 -0500

    Arrays

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 var person = {
     name: 'JuanSbonora',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobbies = _a[_i];
+    console.log(hobbies.toUpperCase());
+}
+/*const product = {
+    id: '001',
+    price: 7.0,
+    tags: ['great-offter','hot-and-new'],
+    details:{
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+    }
+}
+console.log(person,product)*/
--- app.ts
+++ app.ts
@@ -1,5 +1,24 @@
 const person = {
     name: 'JuanSbonora',
-    age: 30
+    age: 30,
+    hobbies: ['Sports','Cooking']
 };
+
+let favoriteActivities: string [];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (let hobbies of person.hobbies) {
+    console.log(hobbies.toUpperCase());
+}
+/*const product = {
+    id: '001',
+    price: 7.0,
+    tags: ['great-offter','hot-and-new'],
+    details:{
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+    }
+}
+console.log(person,product)*/
